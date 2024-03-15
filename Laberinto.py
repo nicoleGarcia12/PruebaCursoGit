4a93cad59a141ca53bbceafcf52b86596cc0e30b
Nicole Garcia <fernandan.garcia@edu.uag.mx>
2024-03-15 12:38:33 -0600

    El setup inicial del proyecto

---
+++ Laberinto.py
@@ -0,0 +1,89 @@
+import random 
+# 15/03/2024
+
+
+FILAS = 5 
+
+COLUMNAS = 5 
+
+META_FILA = FILAS - 1 
+
+META_COLUMNA = COLUMNAS - 1 
+
+def main(): 
+
+    laberinto = [[False] * COLUMNAS for _ in range(FILAS)] 
+
+    fila_jugador, columna_jugador = 0, 0 
+
+    # Inicializar el laberinto 
+
+    laberinto[META_FILA][META_COLUMNA] = True  # Meta 
+
+    # Bucle principal del juego 
+
+    while True: 
+
+        # Imprimir el laberinto 
+
+        for i in range(FILAS): 
+
+            for j in range(COLUMNAS): 
+
+                if i == fila_jugador and j == columna_jugador: 
+
+                    print("X ", end='')  # Jugador 
+
+                elif laberinto[i][j]: 
+
+                    print("O ", end='')  # Meta 
+
+                else: 
+
+                    print(". ", end='')  # Espacio vacío 
+
+            print() 
+
+        # Verificar si el jugador alcanzó la meta 
+
+        if fila_jugador == META_FILA and columna_jugador == META_COLUMNA: 
+
+            print("\n¡Has alcanzado la meta! ¡Felicidades!") 
+
+            break 
+
+        # Solicitar al jugador el siguiente movimiento 
+
+        movimiento = input("\nIngrese su siguiente movimiento (w: arriba, s: abajo, a: izquierda, d: derecha): ") 
+
+ 
+
+        # Mover al jugador según la entrada del usuario 
+
+        if movimiento == 'w' and fila_jugador > 0: 
+
+            fila_jugador -= 1 
+
+        elif movimiento == 's' and fila_jugador < FILAS - 1: 
+
+            fila_jugador += 1 
+
+        elif movimiento == 'a' and columna_jugador > 0: 
+
+            columna_jugador -= 1 
+
+        elif movimiento == 'd' and columna_jugador < COLUMNAS - 1: 
+
+            columna_jugador += 1 
+
+        else: 
+
+            print("Movimiento no válido.") 
+
+ 
+
+if __name__ == "__main__": 
+
+    main() 
+
+ 
---
+++ LaberintoFunciones.py
@@ -0,0 +1,70 @@
+import random
+
+# 15/03/2024
+
+FILAS = 5
+COLUMNAS = 5
+META_FILA = FILAS - 1
+META_COLUMNA = COLUMNAS - 1
+
+def inicializar_laberinto():
+    laberinto = [[False] * COLUMNAS for _ in range(FILAS)]
+    laberinto[META_FILA][META_COLUMNA] = True  # Meta
+    return laberinto
+
+def imprimir_laberinto(laberinto, fila_jugador, columna_jugador):
+    for i in range(FILAS):
+        for j in range(COLUMNAS):
+            if i == fila_jugador and j == columna_jugador:
+                print("X ", end='')  # Jugador
+            elif laberinto[i][j]:
+                print("O ", end='')  # Meta
+            else:
+                print(". ", end='')  # Espacio vacío
+        print()
+
+def verificar_meta(fila_jugador, columna_jugador):
+    return fila_jugador == META_FILA and columna_jugador == META_COLUMNA
+
+def obtener_movimiento(cScheme):
+    return input(f"\nIngrese su siguiente movimiento ({cScheme[0]}: arriba, {cScheme[1]}: abajo, {cScheme[2]}: izquierda, {cScheme[3]}: derecha)" )
+
+def mover_jugador(fila_jugador, columna_jugador, movimiento, cScheme):
+    if movimiento == cScheme[0] and fila_jugador > 0:
+        fila_jugador -= 1
+    elif movimiento == cScheme[1] and fila_jugador < FILAS - 1:
+        fila_jugador += 1
+    elif movimiento == cScheme[2] and columna_jugador > 0:
+        columna_jugador -= 1
+    elif movimiento == cScheme[3] and columna_jugador < COLUMNAS - 1:
+        columna_jugador += 1
+    else:
+        print("Movimiento no válido.")
+    return fila_jugador, columna_jugador
+
+def defineControls(cScheme):
+    cScheme[0] = str(input('Ingrese tecla para moverse hacia arriba.'))
+    cScheme[1] = str(input('Ingrese tecla para moverse hacia abajo.'))
+    cScheme[2] = str(input('Ingrese tecla para moverse hacia la izquierda.'))
+    cScheme[3] = str(input('Ingrese tecla para moverse hacia la derecha.'))
+
+    return cScheme
+
+def main():
+    cScheme = [None] * 4 # array is initialized
+    defineControls(cScheme)
+
+    laberinto = inicializar_laberinto()
+    fila_jugador, columna_jugador = 0, 0
+
+    while True:
+        imprimir_laberinto(laberinto, fila_jugador, columna_jugador)
+        if verificar_meta(fila_jugador, columna_jugador):
+            print("\n¡Has alcanzado la meta! ¡Felicidades!")
+            break
+
+        movimiento = obtener_movimiento(cScheme)
+        fila_jugador, columna_jugador = mover_jugador(fila_jugador, columna_jugador, movimiento, cScheme)
+
+if __name__ == "__main__":
+    main()
